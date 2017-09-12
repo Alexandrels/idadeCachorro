22217549e8a507db38ab053b21888ff8e2efbb90
Alexandre Silveira <alexandre@mobilesys.com.br>
2017-09-12 13:26:42 -0300

    Primeiro commit

---
+++ app/src/main/java/w2/com/br/idadedecachorro/MainActivity.java
@@ -0,0 +1,50 @@
+package w2.com.br.idadedecachorro;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edEntrada;
+    private TextView tvResultado;
+    private Button btCalculo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edEntrada = (EditText) findViewById(R.id.editText);
+        tvResultado = (TextView) findViewById(R.id.tv_resultado);
+        btCalculo = (Button) findViewById(R.id.bt_descobrir_idade);
+        btCalculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoDigitado = edEntrada.getText().toString();
+                if (textoDigitado.isEmpty()) {
+
+                } else {
+                    int valorDigitado = Integer.parseInt(textoDigitado);
+                    int resultadoFinal = valorDigitado * 7;
+
+                    limpaCamposEsconeTeclado();
+                    tvResultado.setText("Em cachorrês " + textoDigitado + " anos equivalem a " + resultadoFinal + " anos humanos!");
+                }
+            }
+        });
+
+    }
+
+    public void limpaCamposEsconeTeclado() {
+        edEntrada.setText("");
+        //esconde o teclado
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(edEntrada.getWindowToken(), 0);
+    }
+}
